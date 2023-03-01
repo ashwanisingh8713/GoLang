2652f8c8190b5e2cb8e1aff52c5df112855190e8
ashwani <ashwani.singh@ninestars.in>
2023-03-01 16:31:23 +0530

    Working on DB integration part

--- src/cassandra/cassandra.go
+++ src/cassandra/cassandra.go
@@ -1,39 +1,65 @@
 package main
 
 import (
+	"ServerDrivenUI/src/cassandra/db"
+	"ServerDrivenUI/src/cassandra/db/table"
+	"ServerDrivenUI/src/cassandra/db/table/address"
 	"fmt"
 	"github.com/gocql/gocql"
 	"log"
+	"sync"
 )
 
 func main() {
 	cassandraSetup()
 }
 
-func cassandraSetup() {
-	//cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
-	//cluster := gocql.NewCluster("127.0.0.1", "127.0.0.1", "127.0.0.1")
-	cluster := gocql.NewCluster("192.168.1.6", "192.168.1.6", "192.168.1.6")
-	cluster.Keyspace = "school"
-	cluster.Consistency = gocql.Quorum
-	session, _ := cluster.CreateSession()
-	defer session.Close()
+var once sync.Once
 
-	if err := session.Query(`INSERT INTO tweet (timeline, id, text) VALUES (?, ?, ?)`,
-		"me", gocql.TimeUUID(), "hello world").Exec(); err != nil {
+var dbInstance *gocql.Session
+
+func getInstance() *gocql.Session {
+	if dbInstance == nil {
+		once.Do(
+			func() {
+				fmt.Println("Creating KEYSPACE instance now.")
+				cluster := gocql.NewCluster(db.HOST_IP)
+				cluster.Keyspace = db.KEYSPACE
+				cluster.Consistency = gocql.Quorum
+				session, _ := cluster.CreateSession()
+				defer session.Close()
+			})
+	} else {
+		fmt.Println("KEYSPACE instance already created.")
+	}
+
+	return dbInstance
+}
+
+func cassandraSetup() {
+	if err := dbInstance.Query(`INSERT INTO `+table.TABLE_Address+`(
+		`+address.AddressId+`,
+		`+address.UserId+`,
+		`+address.IsPreferred+`,
+		`+address.Zip+`,
+		`+address.AddressLine1+`,
+		`+address.AddressType+`,
+		`+address.City+`
+		) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+		gocql.TimeUUID(), "3343", true, "560078", "Shamanna Building", "Home", "Bangalore").Exec(); err != nil {
 		log.Fatal(err)
 	}
 
 	var id gocql.UUID
 	var text string
 
-	if err := session.Query(`SELECT id, text FROM tweet WHERE timeline = ? LIMIT 1`,
+	if err := dbInstance.Query(`SELECT id, text FROM tweet WHERE timeline = ? LIMIT 1`,
 		"me").Consistency(gocql.One).Scan(&id, &text); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Tweet:", id, text)
 
-	iter := session.Query(`SELECT id, text FROM tweet WHERE timeline = ?`, "me").Iter()
+	iter := dbInstance.Query(`SELECT id, text FROM tweet WHERE timeline = ?`, "me").Iter()
 	for iter.Scan(&id, &text) {
 		fmt.Println("Tweet:", id, text)
 	}
---
+++ src/cassandra/db/table/table.go
@@ -0,0 +1,17 @@
+package table
+
+const (
+	TABLE_Address       = "address"
+	TABLE_Cart_item     = "cart_item"
+	TABLE_Category      = "category"
+	TABLE_Coupon        = "coupon"
+	TABLE_Manufacturer  = "manufacturer"
+	TABLE_Order_item    = "order_item"
+	TABLE_Order_status  = "order_status"
+	TABLE_Payment       = "payment"
+	TABLE_Products      = "products"
+	TABLE_Seller        = "seller"
+	TABLE_Sub_category  = "sub_category"
+	TABLE_Subscriptions = "subscriptions"
+	TABLE_User          = "user"
+)

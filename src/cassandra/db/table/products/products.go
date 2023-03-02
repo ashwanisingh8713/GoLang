a6a58b7586ad7151b7f8bedcbfd1b5f004a165d8
ashwani <ashwani.singh@ninestars.in>
2023-03-02 16:31:46 +0530

    Stuck with not updating field, so removing clustering key

--- src/cassandra/db/table/products/products.go
+++ src/cassandra/db/table/products/products.go
@@ -14,7 +14,7 @@ func ProductInsertQuery(session *gocql.Session, sellerId string, sellerName stri
 		"",
 		"Coriander Leaves",
 		"https://www.bigbasket.com/media/uploads/p/s/10000097_19-fresho-coriander-leaves.jpg",
-		0.9000,
+		0.000,
 		"Coriander leaves are green, fragile with a decorative appearance. They contain minimal aroma and have a spicy sweet taste.",
 		sellerId,
 		sellerName,
@@ -45,46 +45,61 @@ const (
 )
 
 type Product struct {
-	ProductId      gocql.UUID
-	CategoryId     string
-	Dimension      string
-	ManufacturerId string
-	Name           string
-	Picture        string
-	Price          float32
-	Description    string
-	SellerId       string
-	SellerName     string
-	Sku            string
-	SubCategoryId  string
-	Weight         float32
-	Units          int
-	LastUpdated    time.Time
-	Subscribable   bool
+	ProductId      gocql.UUID //1
+	CategoryId     string     //2
+	Dimension      string     //3
+	ManufacturerId string     //4
+	Name           string     //5
+	Picture        string     //6
+	Price          float32    //7
+	Description    string     //8
+	SellerId       string     //9
+	SellerName     string     //10
+	Sku            string     //11
+	SubCategoryId  string     //12
+	Weight         float32    //13
+	Units          int        //14
+	LastUpdated    time.Time  //15
+	Subscribable   bool       //16
 }
 
 func Insert(session *gocql.Session, pCategoryId string, pDimension string, pManufacturerId string,
 	pName string, pPicture string, pPrice float32, pDescription string, pSellerId string, pSellerName string, pSku string,
 	pSubCategoryId string, pWeight float32, pUnits int, pSubscribable bool) {
 
+	println("category_id", pCategoryId)
+	println("dimension", pDimension)
+	println("manufacturer_id", pManufacturerId)
+	println("name", pName)
+	println("picture", pPicture)
+	println("price", pPrice)
+	println("description", pDescription)
+	println("seller_id", pSellerId)
+	println("seller_name", pSellerName)
+	println("sku", pSku)
+	println("sub_category_id", pSubCategoryId)
+	println("weight", pWeight)
+	println("units", pUnits)
+	println("subscribable", pSubscribable)
+
 	if err := session.Query(`INSERT INTO `+table.TABLE_Products+`(
-		`+product_id+`,
-		`+category_id+`,
-		`+dimension+`,
-		`+manufacturer_id+`,
-		`+name+`,
-		`+picture+`,
-		`+price+`,
-		`+description+`,
-		`+seller_id+`,
-		`+seller_name+`,
-		`+sku+`,
-		`+sub_category_id+`,
-		`+weight+`,
-		`+units+`,
-		`+last_updated+`,
-		`+subscribable+`
-		) VALUES (?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?,?,?,?,?)`,
+		`+product_id+`,		//1
+		`+category_id+`,	//2
+		`+dimension+`,		//3
+		`+manufacturer_id+`,	//4
+		`+name+`,			//5
+		`+picture+`,		//6
+		`+price+`,			//7
+		`+description+`,	//8
+		`+seller_id+`,		//9
+		`+seller_name+`,	//10
+		`+sku+`,			//11
+		`+sub_category_id+`,//12
+		`+weight+`,			//13
+		`+units+`,			//14
+		`+last_updated+`,	//15
+		`+subscribable+`	//16
+		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)`,
 		gocql.TimeUUID(),
 		pCategoryId,
 		pDimension,
--- src/cassandra/db/table/table.go
+++ src/cassandra/db/table/table.go
@@ -1,17 +1,17 @@
 package table
 
 const (
-	TABLE_Address       = "address"
+	TABLE_Address       = "address" // In Progress
 	TABLE_Cart_item     = "cart_item"
-	TABLE_Category      = "category"
+	TABLE_Category      = "category" // In Progress
 	TABLE_Coupon        = "coupon"
 	TABLE_Manufacturer  = "manufacturer"
 	TABLE_Order_item    = "order_item"
 	TABLE_Order_status  = "order_status"
 	TABLE_Payment       = "payment"
 	TABLE_Products      = "products"
-	TABLE_Seller        = "seller"
-	TABLE_Sub_category  = "sub_category"
+	TABLE_Seller        = "seller"       // In Progress
+	TABLE_Sub_category  = "sub_category" // In Progress
 	TABLE_Subscriptions = "subscriptions"
 	TABLE_User          = "user"
 )

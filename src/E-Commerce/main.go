5a46d9abfc6f5589f26720781fa8a76821c12d64
Ashwani <ashwanisingh8713@gmail.com>
2023-03-15 11:59:24 +0530

    Refactored main.go

--- src/E-Commerce/main.go
+++ src/E-Commerce/main.go
@@ -48,6 +48,21 @@ func main() {
 	// Swagger UI http://192.168.13.119:8080/swagger/index.html
 	route.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
+	// Images Related Static APIs
+	route.Static(ec_constant.BakeryItemsRelativePath4x, ec_constant.BakeryItemsFolderPath4x)     // Bakery Items
+	route.Static(ec_constant.DairyProductsRelativePath4x, ec_constant.DairyProductsFolderPath4x) // Dairy Products
+	route.Static(ec_constant.DealOfTheWeekRelativePath4x, ec_constant.DealOfTheWeekFolderPath4x) // Deal Of the Week
+	route.Static(ec_constant.FeaturedItemsRelativePath4x, ec_constant.FeaturedItemsFolderPath4x) // Featured Items
+	route.Static(ec_constant.FruitsRelativePath4x, ec_constant.FruitsFolderPath4x)               // Fruits
+	route.Static(ec_constant.GroceriesRelativePath4x, ec_constant.GroceriesFolderPath4x)         // Groceries
+	route.Static(ec_constant.TopCategoriesRelativePath4x, ec_constant.TopCategoriesFolderPath4x) // Top Categories
+	route.Static(ec_constant.TopProductsRelativePath4x, ec_constant.TopProductsFolderPath4x)     // Top Products
+	route.Static(ec_constant.TopViewpagerRelativePath4x, ec_constant.TopViewpagerFolderPath4x)   // Top Viewpager
+	route.Static(ec_constant.VegetablesRelativePath4x, ec_constant.VegetablesFolderPath4x)       // Vegetables
+	route.Static(ec_constant.ProfileRelativePath4x, ec_constant.ProfileFolderPath4x)             // Profile
+	route.Static(ec_constant.OnBoardingRelativePath4x, ec_constant.OnBoardingFolderPath4x)       //OnBoarding
+
+	// UI Related APIs
 	route.GET(ec_constant.ROUTE_HOME, response.Home)
 	route.GET(ec_constant.ROUTE_EXPLORE, response.ExploreTopCategory)
 	route.GET(ec_constant.ROUTE_PRODUCT, response.ExploreTopCategory)
@@ -55,33 +70,7 @@ func main() {
 	route.GET(ec_constant.ROUTE_SUBSCRIPTION, response.Subscription)
 	route.GET(ec_constant.ROUTE_ONBOARDING, response.OnBoarding)
 
-	// Bakery Items
-	route.Static(ec_constant.BakeryItemsRelativePath4x, ec_constant.BakeryItemsFolderPath4x)
-	// Dairy Products
-	route.Static(ec_constant.DairyProductsRelativePath4x, ec_constant.DairyProductsFolderPath4x)
-	// Deal Of the Week
-	route.Static(ec_constant.DealOfTheWeekRelativePath4x, ec_constant.DealOfTheWeekFolderPath4x)
-	// Featured Items
-	route.Static(ec_constant.FeaturedItemsRelativePath4x, ec_constant.FeaturedItemsFolderPath4x)
-	// Fruits
-	route.Static(ec_constant.FruitsRelativePath4x, ec_constant.FruitsFolderPath4x)
-	// Groceries
-	route.Static(ec_constant.GroceriesRelativePath4x, ec_constant.GroceriesFolderPath4x)
-	// Top Categories
-	route.Static(ec_constant.TopCategoriesRelativePath4x, ec_constant.TopCategoriesFolderPath4x)
-	// Top Products
-	route.Static(ec_constant.TopProductsRelativePath4x, ec_constant.TopProductsFolderPath4x)
-	// Top Viewpager
-	route.Static(ec_constant.TopViewpagerRelativePath4x, ec_constant.TopViewpagerFolderPath4x)
-	// Vegetables
-	route.Static(ec_constant.VegetablesRelativePath4x, ec_constant.VegetablesFolderPath4x)
-	// Profile
-	route.Static(ec_constant.ProfileRelativePath4x, ec_constant.ProfileFolderPath4x)
-
-	//OnBoarding
-	route.Static(ec_constant.OnBoardingRelativePath4x, ec_constant.OnBoardingFolderPath4x)
-
-	// User Related Details
+	// User Related APIs
 	route.POST("/user", userData.GetUserInfo)
 	route.POST("/address", userData.GetUserAddress)
 	route.POST("/cart_item", userData.GetUserCartItem)
